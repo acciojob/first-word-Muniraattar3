0667f476a48b07c2c2fb6b563cdf7fb6a344ad4b
Munira Yusuf Ali Attar <124035031+Muniraattar3@users.noreply.github.com>
2024-12-03 19:00:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function firstWord(s) {
-  // your code here
+  // Trim any leading or trailing whitespace
+  s = s.trim();
+
+  // Find the index of the first space
+  const firstSpaceIndex = s.indexOf(" ");
+
+  // If there's no space, return the entire string
+  if (firstSpaceIndex === -1) {
+    return s;
+  }
+
+  // Otherwise, return the substring up to the first space
+  return s.substring(0, firstSpaceIndex);
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
